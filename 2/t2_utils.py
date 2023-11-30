98e4010b9307f0d7d0c58883e226187b29f61a10
Richard Hoang <richard.hoang@gmx.at>
2023-11-30 02:21:19 +0100

    trying to optimize ndcg

--- 2/t2_utils.py
+++ 2/t2_utils.py
@@ -11,12 +11,12 @@ def cos_sim(query: [int], target: [int]) -> int:
 def euc_sim(query: [int], target: [int]) -> int:
     return 1/(1+norm(query-target))
 
-def song_retrieval(info: pd.DataFrame, feature: pd.DataFrame, title: str, artist: str, n: int, sim_func = cos_sim) -> pd.DataFrame:
+def song_retrieval(info: pd.DataFrame, feature: pd.DataFrame, title: str, artist: str, n: int, sim_func = cos_sim, filter = []) -> pd.DataFrame:
     feature_no_id = feature.drop(columns="id") # drop id column for similarity measurement
     query_id = info[(info["artist"] == artist) & (info["song"] == title)]["id"].values[0] # search for query song in info
     query = feature[feature["id"]==query_id].drop(columns="id").values[0] # get feature vector for query song
-    sims = [sim_func(query, target) for target in feature_no_id.values] # compute similarity between query and each target song
-    feature["sim"] = sims # add similiarity column to feature
+    feature["sim"] = feature_no_id.apply(lambda target: sim_func(query, target.values), axis=1) # compute and add similiarity column to feature
     info_feature = pd.merge(info, feature[["id", "sim"]], on="id") # merge feature with info by id
     info_feature_sorted = info_feature.sort_values(by=["sim"], ascending=False) # sort by similiartiy in descending order
-    return info_feature_sorted[["id","artist","song","sim"]][1:n+1] # skips the first row, because it is the query track
+    info_feature_sorted = info_feature_sorted if filter == [] else info_feature_sorted[filter] 
+    return info_feature_sorted[1:n+1] # skips the first row, because it is the query track
